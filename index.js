98bc2f07cdec48e1ec85a3265fe485903b8dded8
CodeSandbox <support@codesandbox.io>
2023-08-28 19:59:43 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let TBot =  require('node-telegram-bot-api')
 let axi = require('axios') 
+let express =require('express')
+let app = express()
+app.get('/',(req,res)=>{
+     res.send("Emediongs bot is live..")
+})
+app.listen(3000)
 let token ="6378292408:AAHCLX1ml56YTPOjS6y6sOP5QjfDdf5V99g"
 let bot = new TBot(token,{polling:true})
 bot.on('message', (message)=>{
@@ -7,14 +13,27 @@ let chatId=message.chat.id
  
 async function dictionary(){
    let word=message.text.toLowerCase() 
-  let api=` https://api.dictionaryapi.dev/api/v2/entries/en/${word}` 
-  let raw = await axi.get(api)
-  let main=await raw.data
+  let api=` https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  let raw ="", main =""
+  try{
+   raw = await axi.get(api)
+   main=await raw.data
   let meaning = main[0].meanings[0].definitions[0].definition
-  bot.sendMessage(chatId,"Input another word to get its meaning")
-  bot.sendMessage(chatId,`Meaning : ${meaning} \n \n ${message.from.first_name} try this https://naipsouz.net/4/6142593 `)
+  bot.sendMessage(chatId,`Meaning of ${message.text} : ${meaning} \n \n ${message.from.first_name} try this https://naipsouz.net/4/6142593 `)
+  setTimeout(()=>{
+     bot.sendMessage(chatId,"Input another word to get meaning")
+  },1000)
+  }catch(err){
+     bot.sendMessage(chatId,"Could not find meaning 😩")
+  }
 }
-
+let occ = message.text.search(/\s+/)
+if(message.text=="/start"){
+     bot.sendMessage(chatId,"Input a word to get meaning")
+}
+else if (occ==-1){
      dictionary()
-
+}else{
+     bot.sendMessage(chatId,'Input a correct word without space')
+}
 })
